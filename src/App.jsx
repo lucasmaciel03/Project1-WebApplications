5f3419bdaa464690f7653e40f7b7f7bbeaf3fa47
Lucas Maciel <03lucasmaciel@gmail.com>
2022-10-19 00:40:57 +0100

    feat: add components and routes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {Routes} from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+function App() {
+    return(
+        <div className="App">
+            <Navbar />
+            <Routes>
+
+            </Routes>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
